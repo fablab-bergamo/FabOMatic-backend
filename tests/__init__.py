f0c6be600230950c7cb4d5c95325cf9b8b9f21f5
Pascal Brunot <pascal.brunot@gmail.com>
2023-05-11 20:25:25 +0200

    removed mid parameters, MQTT topic has it already

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 def configure_logger():
     # Create a logger object
-    logger = logging.getLogger()
+    logger = logging.getLogger('test_logger')
     logger.setLevel(logging.DEBUG)
 
     # Create a formatter for the logs
@@ -15,7 +15,7 @@ def configure_logger():
     # Create a rotating file handler with a maximum size of 1 MB
     log_file = os.path.join(os.path.dirname(__file__), 'test-log.txt')
     file_handler = RotatingFileHandler(
-        log_file, maxBytes=1000000, backupCount=1)
+        log_file, maxBytes=1000000, backupCount=1, encoding='latin-1')
     file_handler.setFormatter(formatter)
 
     # Add the file handler to the logger object
