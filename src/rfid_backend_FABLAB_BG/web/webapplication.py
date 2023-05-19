7e79bb45470db72cb008f3414494660eb6f4b8f1
Pascal Brunot <pascal.brunot@gmail.com>
2023-05-19 21:55:34 +0200

    WIP flask pages
    debug and adding machinetypes routes
    added maintenances pages, use history
    
    bugfixes

--- src/rfid_backend_FABLAB_BG/web/webapplication.py
+++ src/rfid_backend_FABLAB_BG/web/webapplication.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+import math
+import re
 import os
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from rfid_backend_FABLAB_BG.database.models import (
@@ -20,10 +23,19 @@ FLASK_TEMPLATES_FOLDER = os.path.join(MODULE_DIR, "flask_app", "templates")
 FLASK_STATIC_FOLDER = os.path.join(MODULE_DIR, "flask_app", "static")
 
 app = Flask(__name__, template_folder=FLASK_TEMPLATES_FOLDER, static_folder=FLASK_STATIC_FOLDER)
+app.config["SECRET_KEY"] = getSetting("web", "secret_key")
 
 engine = create_engine(getSetting("database", "url"), echo=False)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
+
+
+# Custom filter for datetime formatting
+@app.template_filter("datetimeformat")
+def datetimeformat(value, format="%b %d, %Y %I:%M %p"):
+    if type(value) is not datetime:
+        value = datetime.fromtimestamp(value)
+    return value.strftime(format)
 
 
 # Define routes
@@ -32,9 +44,183 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/machines/history/<int:machine_id>", methods=["GET"])
+def view_machine_use_history(machine_id):
+    session = DBSession()
+    uses = session.query(Use).filter_by(machine_id=machine_id).order_by(Use.start_timestamp).all()
+    return render_template("view_machine_use_history.html", uses=uses)
+
+
+@app.route("/delete_use/<int:use_id>", methods=["POST"])
+def delete_use(use_id):
+    session = DBSession()
+    use = session.query(Use).filter_by(use_id=use_id).one()
+    if use:
+        session.delete(use)
+        session.commit()
+        flash("Use deleted successfully.")
+    else:
+        return "Use not found.", 404
+    return redirect(url_for("view_uses"))
+
+
+@app.route("/view_uses", methods=["GET"])
+def view_uses():
+    session = DBSession()
+    user_id = request.args.get("user_id")
+    machine_id = request.args.get("machine_id")
+    start_time = request.args.get("start_time")
+
+    # Convert start_time to a datetime object
+    if start_time:
+        start_time = datetime.strptime(start_time, "%Y-%m-%dT%H:%M")
+
+    # Query the database to get the data
+    uses = session.query(Use)
+
+    if user_id:
+        uses = uses.filter(Use.user_id == user_id)
+
+    if machine_id:
+        uses = uses.filter(Use.machine_id == machine_id)
+
+    if start_time:
+        uses = uses.filter(Use.start_timestamp >= start_time)
+
+    uses = uses.all()
+
+    # Query the database to get all users and machines for the filter dropdowns
+    all_users = session.query(User).all()
+    all_machines = session.query(Machine).all()
+
+    return render_template(
+        "view_uses.html",
+        uses=uses,
+        all_users=all_users,
+        all_machines=all_machines,
+        selected_user_id=user_id,
+        selected_machine_id=machine_id,
+        selected_start_time=start_time,
+    )
+
+
+@app.route("/logout")
+def logout():
+    # here you would add your logout logic, like clearing session, etc.
+    # then redirect to the login page (or wherever you want)
+    return redirect(url_for("login"))
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/maintenances")
+def maintenances():
+    session = DBSession()
+    maintenances = session.query(Maintenance).all()
+    return render_template("view_maintenances.html", maintenances=maintenances)
+
+
+@app.route("/maintenances/add", methods=["GET", "POST"])
+def add_maintenance():
+    session = DBSession()
+    if request.method == "POST":
+        hours_between = request.form["hours_between"]
+        description = request.form["description"]
+        machine_id = request.form["machine_id"]
+        maintenance = Maintenance(hours_between=hours_between, description=description, machine_id=machine_id)
+        session.add(maintenance)
+        session.commit()
+        return redirect(url_for("maintenances"))
+    else:
+        machines = session.query(Machine).all()
+        return render_template("add_maintenance.html", machines=machines)
+
+
+@app.route("/maintenances/edit/<int:maintenance_id>", methods=["GET", "POST"])
+def edit_maintenance(maintenance_id):
+    session = DBSession()
+    maintenance = session.query(Maintenance).filter_by(maintenance_id=maintenance_id).one()
+    if request.method == "POST":
+        maintenance.hours_between = request.form["hours_between"]
+        maintenance.description = request.form["description"]
+        maintenance.machine_id = request.form["machine_id"]
+        session.add(maintenance)
+        session.commit()
+        return redirect(url_for("maintenances"))
+    else:
+        machines = session.query(Machine).all()
+        return render_template(
+            "edit_maintenance.html",
+            machines=machines,
+            maintenance_id=maintenance.maintenance_id,
+            maintenance=maintenance,
+        )
+
+
+@app.route("/maintenances/delete/<int:maintenance_id>", methods=["GET", "POST"])
+def delete_maintenance(maintenance_id):
+    session = DBSession()
+    maintenance = session.query(Maintenance).filter_by(maintenance_id=maintenance_id).one()
+    if not maintenance:
+        return "Maintenance not found", 404
+
+    if request.method == "POST":
+        session.delete(maintenance)
+        session.commit()
+        flash("Maintenance deleted successfully.")
+        return redirect(url_for("maintenances"))
+
+    return render_template("delete_maintenance.html", maintenance=maintenance)
+
+
+@app.route("/machinetypes")
+def machinetypes():
+    session = DBSession()
+    machine_types = session.query(MachineType).all()
+    return render_template("view_machinetypes.html", machine_types=machine_types)
+
+
+@app.route("/machinetypes/add", methods=["GET", "POST"])
+def add_machinetype():
+    if request.method == "POST":
+        session = DBSession()
+        type_name = request.form["type_name"]
+        machine_type = MachineType(type_name=type_name)
+        session.add(machine_type)
+        session.commit()
+        return redirect(url_for("machinetypes"))
+    else:
+        return render_template("add_machinetype.html")
+
+
+@app.route("/machinetypes/edit/<int:type_id>", methods=["GET", "POST"])
+def edit_machinetype(type_id):
+    session = DBSession()
+    machine_type = session.query(MachineType).filter_by(type_id=type_id).one()
+    if request.method == "POST":
+        machine_type.type_name = request.form["type_name"]
+        session.add(machine_type)
+        session.commit()
+        return redirect(url_for("machinetypes"))
+    else:
+        return render_template("edit_machinetype.html", type_id=machine_type.type_id, machine_type=machine_type)
+
+
+@app.route("/machinetypes/delete/<int:type_id>", methods=["GET", "POST"])
+def delete_machinetype(type_id):
+    session = DBSession()
+    machine_type = session.query(MachineType).filter_by(type_id=type_id).one()
+    if not machine_type:
+        return "Machine Type not found", 404
+    if request.method == "POST":
+        session.delete(machine_type)
+        session.commit()
+        flash("Machine type deleted successfully.")
+        return redirect(url_for("machinetypes"))
+    return render_template("delete_machinetype.html", machine_type=machine_type)
 
 
 @app.route("/roles")
@@ -82,7 +268,8 @@ def delete_role(role_id):
 
     if request.method == "POST":
         role_repo.delete(role)
-        return redirect(url_for("view_roles"))
+        flash("Role deleted successfully.")
+        return redirect(url_for("roles"))
 
     return render_template("delete_role.html", role=role)
 
@@ -120,34 +307,43 @@ def edit_machine_type(type_id):
 
 @app.route("/users", methods=["GET"])
 def view_users():
-    users = User.query.all()
+    session = DBSession()
+    users = session.query(User).order_by(User.user_id).all()
     return render_template("view_users.html", users=users)
 
 
 @app.route("/users/add", methods=["GET"])
 def add_user():
-    roles = Role.query.all()
+    session = DBSession()
+    roles = session.query(Role).order_by(Role.role_id).all()
     return render_template("add_user.html", roles=roles)
 
 
 @app.route("/users/create", methods=["POST"])
 def create_user():
+    session = DBSession()
     user_data = request.form
+    card_UUID = user_data.get("card_UUID", None)
+    if card_UUID and not re.match(r"^[0-9A-Fa-f]{8}$", card_UUID):
+        flash("Invalid card UUID. Please enter either 8 hexadecimal characters or leave it empty.", "error")
+        return redirect(url_for("view_users"))
+
     new_user = User(
         name=user_data["name"],
         surname=user_data["surname"],
         role_id=user_data["role_id"],
-        card_UUID=user_data.get("card_UUID", None),
+        card_UUID=card_UUID,
     )
-    db.session.add(new_user)
-    db.session.commit()
+    session.add(new_user)
+    session.commit()
     return redirect(url_for("view_users"))
 
 
 @app.route("/users/edit/<int:user_id>", methods=["GET"])
 def edit_user(user_id):
-    user = User.query.get(user_id)
-    roles = Role.query.all()
+    session = DBSession()
+    user = session.query(User).filter_by(user_id=user_id).one()
+    roles = session.query(Role).order_by(Role.role_id).all()
     if user:
         return render_template("edit_user.html", user=user, roles=roles)
     else:
@@ -156,14 +352,20 @@ def edit_user(user_id):
 
 @app.route("/users/update", methods=["POST"])
 def update_user():
+    session = DBSession()
     user_data = request.form
-    user = User.query.get(user_data["user_id"])
+    user = session.query(User).filter_by(user_id=user_data["user_id"]).one()
     if user:
+        # Validate the card_UUID
+        card_UUID = user_data.get("card_UUID", None)
+        if card_UUID and not re.match(r"^[0-9A-Fa-f]{8}$", card_UUID):
+            flash("Invalid card UUID. Please enter either 8 hexadecimal characters or leave it empty.", "error")
+            return redirect(url_for("edit_user", user_id=user.user_id))
         user.name = user_data["name"]
         user.surname = user_data["surname"]
         user.role_id = user_data["role_id"]
         user.card_UUID = user_data.get("card_UUID", None)
-        db.session.commit()
+        session.commit()
         return redirect(url_for("view_users"))
     else:
         return "User not found", 404
@@ -171,13 +373,14 @@ def update_user():
 
 @app.route("/users/delete/<int:user_id>", methods=["GET", "POST"])
 def delete_user(user_id):
-    user = User.query.get(user_id)
+    session = DBSession()
+    user = session.query(User).filter_by(user_id=user_id).one()
     if not user:
         return "User not found", 404
 
     if request.method == "POST":
-        db.session.delete(user)
-        db.session.commit()
+        session.delete(user)
+        session.commit()
         return redirect(url_for("view_users"))
 
     return render_template("delete_user.html", user=user)
@@ -185,34 +388,38 @@ def delete_user(user_id):
 
 @app.route("/machines", methods=["GET"])
 def view_machines():
-    machines = Machine.query.all()
+    session = DBSession()
+    machines = session.query(Machine).order_by(Machine.machine_id).all()
     return render_template("view_machines.html", machines=machines)
 
 
 @app.route("/machines/add", methods=["GET"])
 def add_machine():
-    machine_types = MachineType.query.all()
+    session = DBSession()
+    machine_types = session.query(MachineType).order_by(MachineType.type_id).all()
     return render_template("add_machine.html", machine_types=machine_types)
 
 
 @app.route("/machines/create", methods=["POST"])
 def create_machine():
+    session = DBSession()
     machine_data = request.form
     new_machine = Machine(
         machine_name=machine_data["machine_name"],
         machine_type_id=machine_data["machine_type_id"],
         machine_hours=float(machine_data["machine_hours"]),
-        blocked=machine_data.get("blocked", False),
+        blocked=machine_data.get("blocked", "off") == "on",
     )
-    db.session.add(new_machine)
-    db.session.commit()
+    session.add(new_machine)
+    session.commit()
     return redirect(url_for("view_machines"))
 
 
 @app.route("/machines/edit/<int:machine_id>", methods=["GET"])
 def edit_machine(machine_id):
-    machine = Machine.query.get(machine_id)
-    machine_types = MachineType.query.all()
+    session = DBSession()
+    machine = session.query(Machine).filter_by(machine_id=machine_id).one()
+    machine_types = session.query(MachineType).order_by(MachineType.type_id).all()
     if machine:
         return render_template("edit_machine.html", machine=machine, machine_types=machine_types)
     else:
@@ -221,14 +428,15 @@ def edit_machine(machine_id):
 
 @app.route("/machines/update", methods=["POST"])
 def update_machine():
+    session = DBSession()
     machine_data = request.form
-    machine = Machine.query.get(machine_data["machine_id"])
+    machine = session.query(Machine).filter_by(machine_id=machine_data["machine_id"]).one()
     if machine:
         machine.machine_name = machine_data["machine_name"]
         machine.machine_type_id = machine_data["machine_type_id"]
         machine.machine_hours = float(machine_data["machine_hours"])
-        machine.blocked = machine_data.get("blocked", False)
-        db.session.commit()
+        machine.blocked = machine_data.get("blocked", "off") == "on"
+        session.commit()
         return redirect(url_for("view_machines"))
     else:
         return "Machine not found", 404
