0df21edd43dd7045e99314b508d17bf8bc632a33
Pascal Brunot <pascal.brunot@gmail.com>
2024-01-12 21:12:04 +0100

    added ssl support (issue #3 )

--- src/rfid_backend_FABLAB_BG/main.py
+++ src/rfid_backend_FABLAB_BG/main.py
@@ -54,7 +54,7 @@ _flaskThread: threading.Thread = None
 def _startApp() -> None:
     from rfid_backend_FABLAB_BG.web.webapplication import app
 
-    app.run(host="0.0.0.0", port=23336, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=23336, debug=True, use_reloader=False, ssl_context="adhoc")
 
 
 def startServer() -> None:
