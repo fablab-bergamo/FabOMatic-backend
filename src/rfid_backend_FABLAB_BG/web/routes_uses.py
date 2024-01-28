1db5f400535a588408af204b79b09a9e33b358be
Pascal Brunot <pascal.brunot@gmail.com>
2024-01-28 22:04:09 +0100

    History: added manual registration of uses

--- src/rfid_backend_FABLAB_BG/web/routes_uses.py
+++ src/rfid_backend_FABLAB_BG/web/routes_uses.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from rfid_backend_FABLAB_BG.database.models import Machine, Use, User
+from rfid_backend_FABLAB_BG.database.repositories import MachineRepository, UserRepository
 from .webapplication import DBSession, app
 
 
@@ -74,3 +75,57 @@ def view_uses():
         selected_machine_id=machine_id,
         selected_start_time=start_time,
     )
+
+
+@app.route("/add_use", methods=["GET"])
+@login_required
+def add_use():
+    with DBSession() as session:
+        users = session.query(User).filter_by(deleted=False).order_by(User.name).all()
+        machines = session.query(Machine).order_by(Machine.machine_name).all()
+        return render_template("add_use.html", users=users, machines=machines)
+
+
+@app.route("/add_use", methods=["POST"])
+@login_required
+def add_use_post():
+    with DBSession() as session:
+        use_data = request.form
+        user_id = use_data["user_id"]
+        machine_id = use_data["machine_id"]
+        try:
+            start_timestamp = datetime.strptime(use_data["start_timestamp"], "%Y-%m-%dT%H:%M")
+            start_timestamp = start_timestamp.timestamp()
+        except ValueError:
+            flash("Invalid start timestamp.", "error")
+            return redirect(url_for("add_use"))
+
+        try:
+            end_timestamp = datetime.strptime(use_data["end_timestamp"], "%Y-%m-%dT%H:%M")
+            end_timestamp = end_timestamp.timestamp()
+        except ValueError:
+            end_timestamp = None
+
+        machine_repo = MachineRepository(session)
+        machine = machine_repo.get_by_id(machine_id)
+        if machine is None:
+            flash("Machine not found.", "error")
+            return redirect(url_for("add_use"))
+
+        user_repo = UserRepository(session)
+        user = user_repo.get_by_id(user_id)
+        if user is None:
+            flash("User not found.", "error")
+            return redirect(url_for("add_use"))
+
+        new_use = Use(
+            user_id=user_id,
+            machine_id=machine_id,
+            start_timestamp=start_timestamp,
+            last_seen=start_timestamp,
+            end_timestamp=end_timestamp,
+        )
+        session.add(new_use)
+        session.commit()
+        flash("Registration added successfully.")
+        return redirect(url_for("view_uses"))
