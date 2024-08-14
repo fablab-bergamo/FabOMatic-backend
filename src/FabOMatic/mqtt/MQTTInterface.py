dd1d0dabd350ccbb3ecd9b01c766713e01baf256
Pascal Brunot <pascal.brunot@gmail.com>
2024-08-14 13:05:16 +0200

    New optional setting request_subtopic

--- src/FabOMatic/mqtt/MQTTInterface.py
+++ src/FabOMatic/mqtt/MQTTInterface.py
@@ -50,13 +50,18 @@ class MQTTInterface:
         """
         Loads the MQTT settings from the settings file.
         """
-        settings = toml.load(self._settings_path)["MQTT"]
+        settings:dict = toml.load(self._settings_path)["MQTT"]
         self._broker = settings["broker"]
         self._port = settings["port"]
         self._client_id = settings["client_id"]
         self._topic = settings["topic"]
         self._reply_subtopic = settings["reply_subtopic"]
-        self._request_subtopic = settings["request_subtopic"]
+        
+        if ("request_subtopic" in settings.keys()):
+            self._request_subtopic = settings["request_subtopic"]
+        else
+            self._request_subtopic = "/request"
+
         self._statsTopic = settings["stats_topic"] + "/" + self._client_id
         logging.info("Loaded MQTT settings from file %s", self._settings_path)
 
